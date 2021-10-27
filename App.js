9a38e4406686e06f6e93664e664bb1e403ff22a5
Junior6155 <hjowusu@st.ug.edu.gh>
2021-10-27 21:14:54 +0000

    Working on the screens

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./screens/Login";
+import Home from "./screens/Home";
 
 export default function App() {
+  const MainNavigator = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <NavigationContainer>
+        <MainNavigator.Navigator initialRouteName="Login">
+          <MainNavigator.Screen name="Login" component={Login} />
+          <MainNavigator.Screen name="Home" component={Home} />
+        </MainNavigator.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
@@ -14,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
