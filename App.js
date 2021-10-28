9bf8ad05315279acea5240b80e4336c46f26fc03
Junior6155 <hjowusu@st.ug.edu.gh>
2021-10-28 03:08:06 +0000

    Working  on the screens

--- App.js
+++ App.js
@@ -3,18 +3,17 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import Products from "./screens/Products";
 import Login from "./screens/Login";
-import Home from "./screens/Home";
 
 export default function App() {
   const MainNavigator = createStackNavigator();
-
   return (
     <View>
       <NavigationContainer>
         <MainNavigator.Navigator initialRouteName="Login">
+          <MainNavigator.Screen name="Products" component={Products} />
           <MainNavigator.Screen name="Login" component={Login} />
-          <MainNavigator.Screen name="Home" component={Home} />
         </MainNavigator.Navigator>
       </NavigationContainer>
     </View>
