9dffd0a1f6d4239c490949796ddcea428cb0c8f0
Junior6155 <hjowusu@st.ug.edu.gh>
2021-10-31 23:19:29 +0000

    Update on the Login screen

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./screens/Login";
--- screens/Login.js
+++ screens/Login.js
@@ -1,17 +1,59 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function Login() {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Image
         style={{
-          width: 150,
-          height: 150,
+          width: 200,
+          height: 250,
+          borderRadius: 10,
+          marginBottom: 20,
         }}
-        source={{ uri: "https://unsplash.com/photos/vISNAATFXlE" }}
+        source={{
+          uri: "https://images.unsplash.com/photo-1593359863503-f598684c806f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGhlYWRwaG9uZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
+        }}
       />
-      <Text style={{ color: "black" }}>Hello</Text>
+
+      <TouchableOpacity
+        style={{
+          background: "#008b8b",
+          borderRadius: 10,
+          flexDirection: "row",
+          alignItems: "center",
+          padding: 10,
+          paddingHorizontal: 60,
+          marginTop: 20,
+        }}
+      >
+        <AntDesign name="google" size={24} color="red" />
+
+        <Text
+          style={{
+            color: "black",
+            fontSize: 15,
+            fontWeight: "300",
+            marginLeft: 15,
+          }}
+        >
+          Login with Google
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          background: "#1e90ff",
+          borderRadius: 10,
+          padding: 10,
+          paddingHorizontal: 60,
+          marginTop: 20,
+        }}
+      >
+        <Text style={{ color: "black", fontSize: 15, fontWeight: "300" }}>
+          Continue to sign up
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
