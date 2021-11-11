509996471d79f21b36a3997929d0dd06bfe88d5b
Junior6155 <hjowusu@st.ug.edu.gh>
2021-11-11 16:36:35 +0000

    Fonts and screens updated

---
+++ src/data/all.js
@@ -0,0 +1,136 @@
+const allData = [
+  {
+    id: 1,
+    category: "shoe",
+    name: "Adidas F2",
+    price: 90.0,
+    description: "No description",
+    image: require("../images/shoe/adidas.png"),
+  },
+  {
+    id: 2,
+    name: "Mountain Bike T-Shirt",
+    category: "shirt",
+    price: 45.0,
+    description:
+      "Fits close to the body for fitted, slim silhouette. Smooth and comfortable lightweight jersey fabric.",
+    image: require("../images/shirt/shirt(3).png"),
+  },
+  {
+    id: 3,
+    name: "Nike",
+    category: "shoe",
+    price: 110.0,
+    description:
+      "Men's Fashion Sneakers with Traditional Lace-up Closure design ,tongue and heel pull tab helps with easy on and off, adjustable shoe width freely.",
+    image: require("../images/shoe/nike.png"),
+  },
+  {
+    id: 4,
+    name: "ServisHero T-Shirt",
+    category: "shirt",
+    price: 48.0,
+    description:
+      "Regular Fit,simple and casual, men basic t-shirt, undershirt for men.",
+    image: require("../images/shirt/shirt(5).png"),
+  },
+  {
+    id: 5,
+    name: "Vans",
+    category: "shoe",
+    price: 75.0,
+    description:
+      "These men's Low-top sneakers outsole specially equipped with anti-slip lines Grooves, which Strengthen the Shoes Ability of Anti-Skid and durability.",
+    image: require("../images/shoe/vans2.png"),
+  },
+  {
+    id: 6,
+    name: "High Heel",
+    category: "shoe",
+    price: 80.0,
+    description: "Ladies high heel for that special occasion.",
+    image: require("../images/shoe/fancy.png"),
+  },
+  {
+    id: 7,
+    name: "RX Sunglass",
+    category: "glasses",
+    price: 45.0,
+    description:
+      "Casual frame design keeps you looking professional and stylish while working or taking a stroll on the beach.",
+    image: require("../images/glasses/glass(4).png"),
+  },
+  {
+    id: 8,
+    name: "Vans",
+    category: "shoe",
+    price: 85.0,
+    description:
+      "These men's Low-top sneakers outsole specially equipped with anti-slip lines Grooves, which Strengthen the Shoes Ability of Anti-Skid and durability.",
+    image: require("../images/shoe/vans3.png"),
+  },
+  {
+    id: 9,
+    name: "High Heel",
+    category: "shoe",
+    price: 70.0,
+    description: "Ladies high heel for that special occasion.",
+    image: require("../images/shoe/fancy2.png"),
+  },
+  {
+    id: 10,
+    name: "Pyne S2",
+    category: "shoe",
+    price: 60.0,
+    description:
+      "Provide strong support and anti-reversal function make it safe for you to perform various movements.",
+    image: require("../images/shoe/pyne2.png"),
+  },
+  {
+    id: 11,
+    name: "Calvin Long Sleeve",
+    category: "shirt",
+    price: 55.0,
+    description:
+      "Soft cotton provides ultimate comfort. The fabric is on the medium-to-thin thickness level, not see through.",
+    image: require("../images/shirt/shirt(6).png"),
+  },
+  {
+    id: 12,
+    name: "Nike",
+    category: "shoe",
+    price: 90.0,
+    description:
+      "Men's Fashion Sneakers with Traditional Lace-up Closure design ,tongue and heel pull tab helps with easy on and off, adjustable shoe width freely.",
+    image: require("../images/shoe/nike2.png"),
+  },
+  {
+    id: 13,
+    name: "BALEAF Long Sleeve",
+    category: "shirt",
+    price: 38.0,
+    description:
+      "Thumbholes prevent sleeves from riding up and keep warm during cold weather activities.",
+    image: require("../images/shirt/shirt(8).png"),
+  },
+  {
+    id: 14,
+    name: "Vans",
+    category: "shoe",
+    price: 80.0,
+    description:
+      "These men's Low-top sneakers outsole specially equipped with anti-slip lines Grooves, which Strengthen the Shoes Ability of Anti-Skid and durability.",
+    image: require("../images/shoe/vans.png"),
+  },
+  {
+    id: 15,
+    name: "Pyne",
+    category: "shoe",
+    price: 70.0,
+    description:
+      "Provide strong support and anti-reversal function make it safe for you to perform various movements.",
+    image: require("../images/shoe/pyne.png"),
+  },
+];
+
+export default allData;
